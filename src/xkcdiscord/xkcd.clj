7270cd2ab86f795da4bf49b487342cc2a9998c46
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-06-22 21:08:53 +0200

    Provide workaround for SNI support in http-kit

--- src/xkcdiscord/xkcd.clj
+++ src/xkcdiscord/xkcd.clj
@@ -7,7 +7,18 @@
             [remus :as rss]
             [clojure.string :as string]
             [clojure.java.io :as io])
-  (:import (java.io StringBufferInputStream)))
+  (:import (java.net URI)
+           (javax.net.ssl SNIHostName SSLEngine)))
+
+(defn ssl-configurer [^SSLEngine eng, ^URI uri]
+  (let [host-name (SNIHostName. (.getHost uri))
+        params (doto (.getSSLParameters eng)
+                 (.setServerNames [host-name]))]
+    (doto eng
+      (.setUseClientMode true) ;; required for JDK12/13 but not for JDK1.8
+      (.setSSLParameters params))))
+
+(def http-client (http/make-client {:ssl-configurer ssl-configurer}))
 
 (def base-url "https://xkcd.com")
 
@@ -21,7 +32,7 @@
 (def rss-url (xkcd-url "atom.xml"))
 
 (defn read-rss []
-  (-> @(http/get rss-url)
+  (-> @(http/get rss-url {:client http-client})
       :body
       (.getBytes "UTF-8")
       (io/input-stream)
@@ -31,12 +42,18 @@
 (defn api-url [url]
   (str url "/info.0.json"))
 
+(def headers
+  {"User-Agent" "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
+   "Host" "xkcd.com"
+   "Accept" "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"})
+
 (defn get
   ([] (get nil))
   ([num]
-   (let [{:keys [status body]} @(http/get (api-url (xkcd-url num)))]
-     (when (= status 200)
-       (json/parse-string body keyword)))))
+   (let [{:keys [status body] :as response} @(http/get (api-url (xkcd-url num)) {:client http-client})]
+     (if (= status 200)
+       (json/parse-string body keyword)
+       (ex-info "Request failed" response)))))
 
 (def url-pattern #"https?:\/\/xkcd\.com/(\d+)\/?")
 
@@ -44,14 +61,14 @@
   (clojure.core/get (re-matches url-pattern url) 1 nil))
 
 (defn random []
-  (-> @(http/get random-url {:follow-redirects false})
+  (-> @(http/get random-url {:follow-redirects false :client http-client})
       :headers
       :location
       xkcd-url->num
       get))
 
 (defn read-archive []
-  (->> @(http/get archive-url)
+  (->> @(http/get archive-url {:client http-client})
        :body
        html/parse
        html/as-hickory
