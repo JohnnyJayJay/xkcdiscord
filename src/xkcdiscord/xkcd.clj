97e38ccccb84e4b684239a04f1c52b96989f014a
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-06-23 12:21:56 +0200

    Remove ssl workaround (not needed with latest http-kit anymore)

--- src/xkcdiscord/xkcd.clj
+++ src/xkcdiscord/xkcd.clj
@@ -6,19 +6,7 @@
             [hickory.select :as s]
             [remus :as rss]
             [clojure.string :as string]
-            [clojure.java.io :as io])
-  (:import (java.net URI)
-           (javax.net.ssl SNIHostName SSLEngine)))
-
-(defn ssl-configurer [^SSLEngine eng, ^URI uri]
-  (let [host-name (SNIHostName. (.getHost uri))
-        params (doto (.getSSLParameters eng)
-                 (.setServerNames [host-name]))]
-    (doto eng
-      (.setUseClientMode true) ;; required for JDK12/13 but not for JDK1.8
-      (.setSSLParameters params))))
-
-(def http-client (http/make-client {:ssl-configurer ssl-configurer}))
+            [clojure.java.io :as io]))
 
 (def base-url "https://xkcd.com")
 
@@ -32,28 +20,22 @@
 (def rss-url (xkcd-url "atom.xml"))
 
 (defn read-rss []
-  (-> @(http/get rss-url {:client http-client})
+  (-> @(http/get rss-url)
       :body
       (.getBytes "UTF-8")
       (io/input-stream)
       rss/parse-stream))
 
-
 (defn api-url [url]
   (str url "/info.0.json"))
-
-(def headers
-  {"User-Agent" "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
-   "Host" "xkcd.com"
-   "Accept" "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"})
 
 (defn get
   ([] (get nil))
   ([num]
-   (let [{:keys [status body] :as response} @(http/get (api-url (xkcd-url num)) {:client http-client})]
+   (let [{:keys [status body] :as response} @(http/get (api-url (xkcd-url num)))]
      (if (= status 200)
        (json/parse-string body keyword)
-       (ex-info "Request failed" response)))))
+       (throw (ex-info "Request failed" response))))))
 
 (def url-pattern #"https?:\/\/xkcd\.com/(\d+)\/?")
 
@@ -61,14 +43,14 @@
   (clojure.core/get (re-matches url-pattern url) 1 nil))
 
 (defn random []
-  (-> @(http/get random-url {:follow-redirects false :client http-client})
+  (-> @(http/get random-url {:follow-redirects false})
       :headers
       :location
       xkcd-url->num
       get))
 
 (defn read-archive []
-  (->> @(http/get archive-url {:client http-client})
+  (->> @(http/get archive-url)
        :body
        html/parse
        html/as-hickory
@@ -77,6 +59,8 @@
        (reverse)
        (vec)))
 
+;; Search in transcripts?
+;; How to get that text at scale?
 (defn search [archive query]
   (let [lower-query (string/lower-case query)]
     (filter (comp #(string/includes? % lower-query) string/lower-case second) archive)))
